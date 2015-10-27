39eb4c33c007ab3e037d47e9a176382f72928b33
Gary Ewan Park <gep13@gep13.co.uk>
2015-10-27 22:43:50 +0000

    (GH-3) Added --version switch

---
+++ Source/ReSharperReports/Options/TransformSubOptions.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+
+namespace ReSharperReports.Options
+{
+    [Verb("transform", HelpText = "Transforms an input XML report file.")]
+    public class TransformSubOptions
+    {
+        /// <summary>
+        /// Gets or set the input file path
+        /// </summary>
+        [Option('i', "inputFilePath", HelpText = "The path to the input report XML to be transformed.", Required = true)]
+        public string InputFilePath { get; set; }
+
+        /// <summary>
+        /// Gets or set the output file path
+        /// </summary>
+        [Option('o', "outputFilePath", HelpText = "The path to the output report in HTML format.", Required = true)]
+        public string OutputFilePath { get; set; }
+
+        /// <summary>
+        /// Gets or set the XSL Transformation file path
+        /// </summary>
+        /// /// <remarks>This is an optional input</remarks>
+        [Option('x', "xslFilePath", HelpText = "The path to the input XSL file to be used to perform transformation.", Required = false)]
+        public string XslFilePath { get; set; }
+
+        /// <summary>
+        /// Gets or set the log file path
+        /// </summary>
+        /// <remarks>This is an optional input</remarks>
+        [Option('l', "logFilePath", HelpText = "Path to where log file should be created. Defaults to logging to console.", Required = false)]
+        public string LogFilePath { get; set; }
+    }
+}
--- build.cake
+++ build.cake
@@ -28,7 +28,7 @@ var assemblyInfo        = new AssemblyInfoSettings {
                                 Version                 = version,
                                 FileVersion             = version,
                                 InformationalVersion    = semVersion,
-                                Copyright               = string.Format("Copyright © gep13 {0} - Present", DateTime.Now.Year),
+                                Copyright               = string.Format("Copyright © gep13 and Contributors {0} - Present", DateTime.Now.Year),
                                 CLSCompliant            = true
                             };
 var nuGetPackSettings   = new NuGetPackSettings {
